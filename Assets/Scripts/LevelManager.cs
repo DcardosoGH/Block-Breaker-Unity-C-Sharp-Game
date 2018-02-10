c26dd33260e0a3cea9657dd42da2eef94d14617e
Donwor <diogo.g.c@gmail.com>
2018-02-10 16:13:25 +0000

    first commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string name) {
+		Debug.Log("Level Load Requested for: "+ name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log("I want to quit!");
+		Application.Quit();
+	}
+}
